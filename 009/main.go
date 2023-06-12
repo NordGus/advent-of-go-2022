9569b2aeb6ebc2aecccc8162e75a8427abfa55c4
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2023-06-12 21:10:28 +0000

    implementing day 9 part 1

---
+++ 009/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/NordGus/advent-of-go-2022/009/structs"
+)
+
+const (
+	inputFileName = "009/input.txt"
+)
+
+func main() {
+	file, err := os.Open(inputFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	input := scanInput(file)
+	moves := parseMovements(input)
+	grid := makeMoves(moves)
+
+	g := <-grid
+
+	fmt.Println(g.CountTailUniqueLocations())
+}
+
+func scanInput(input *os.File) <-chan string {
+	out := make(chan string)
+
+	scanner := bufio.NewScanner(input)
+
+	go func(scanner *bufio.Scanner, out chan<- string) {
+		for scanner.Scan() {
+			out <- scanner.Text()
+		}
+
+		close(out)
+	}(scanner, out)
+
+	return out
+}
+
+func parseMovements(input <-chan string) <-chan structs.Movement {
+	out := make(chan structs.Movement)
+
+	go func(input <-chan string, out chan<- structs.Movement) {
+		for in := range input {
+			move := strings.Split(in, " ")
+			amount, err := strconv.ParseInt(move[1], 10, 0)
+			if err != nil {
+				log.Fatalf("error Parsing Movements: %v", err.Error())
+			}
+
+			movement := structs.Movement{
+				Amount: int(amount),
+			}
+
+			switch move[0] {
+			case "R":
+				movement.Direction = structs.RightMovement
+			case "L":
+				movement.Direction = structs.LeftMovement
+			case "U":
+				movement.Direction = structs.UpMovement
+			case "D":
+				movement.Direction = structs.DownMovement
+			default:
+				log.Fatalf("error Parsing Movements: unsupported movement direction")
+			}
+
+			out <- movement
+		}
+
+		close(out)
+	}(input, out)
+
+	return out
+}
+
+func makeMoves(input <-chan structs.Movement) <-chan *structs.Grid {
+	out := make(chan *structs.Grid, 1)
+
+	go func(moves <-chan structs.Movement, out chan<- *structs.Grid) {
+		grid := structs.NewGrid()
+
+		for move := range moves {
+			grid.Move(move)
+		}
+
+		out <- &grid
+
+		close(out)
+	}(input, out)
+
+	return out
+}
