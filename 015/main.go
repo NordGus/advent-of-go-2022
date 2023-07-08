0b951742497c33bcbad0a7abc41e34d186fdadb8
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2023-07-08 03:16:44 +0000

    setting up day 15

---
+++ 015/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+const (
+	inputFileName = "015/input.txt"
+)
+
+func main() {
+	file, err := os.Open(inputFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	input := scanInput(file)
+
+	for in := range input {
+		fmt.Println(in)
+	}
+}
+
+func scanInput(input *os.File) <-chan string {
+	out := make(chan string)
+
+	scanner := bufio.NewScanner(input)
+
+	go func(scanner *bufio.Scanner, out chan<- string) {
+		for scanner.Scan() {
+			out <- scanner.Text()
+		}
+
+		close(out)
+	}(scanner, out)
+
+	return out
+}
