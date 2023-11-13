2a3bf8102b1f1cbaa45dd22584f23e28fa920f6d
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2023-11-13 07:17:42 +0100

    day 19 part 2: optimizing

--- 019/factory/factory.go
+++ 019/factory/factory.go
@@ -1,6 +1,8 @@
 package factory
 
-import "runtime"
+import (
+	"runtime"
+)
 
 type state struct {
 	OreRobots      int
@@ -68,7 +70,7 @@ func (f *Factory) QualityScoreDuring(duration int) int {
 			if current.skipped == robots[i] {
 				produce := tick{state: nextState(current.state, robot{}), time: current.time + 1}
 
-				if !visited[produce.state] {
+				if !visited[produce.state] && produce.state.Geode >= top.state.Geode-1 {
 					states = append(states, produce)
 				}
 
@@ -78,7 +80,7 @@ func (f *Factory) QualityScoreDuring(duration int) int {
 			if f.blueprint.robots[robots[i]].canBeBuilt(current.state.Ore, current.state.Clay, current.state.Obsidian) {
 				build := tick{state: nextState(current.state, f.blueprint.robots[robots[i]]), time: current.time + 1}
 
-				if !visited[build.state] {
+				if !visited[build.state] && build.state.Geode >= top.state.Geode-1 {
 					states = append(states, build)
 				}
 
@@ -90,7 +92,7 @@ func (f *Factory) QualityScoreDuring(duration int) int {
 
 				noBuild := tick{state: nextState(current.state, robot{}), skipped: robots[i], time: current.time + 1}
 
-				if !visited[noBuild.state] {
+				if !visited[noBuild.state] && noBuild.state.Geode >= top.state.Geode-1 {
 					states = append(states, noBuild)
 				}
 			}
@@ -99,7 +101,7 @@ func (f *Factory) QualityScoreDuring(duration int) int {
 		if !built {
 			produce := tick{state: nextState(current.state, robot{}), time: current.time + 1}
 
-			if !visited[produce.state] {
+			if !visited[produce.state] && produce.state.Geode >= top.state.Geode-1 {
 				states = append(states, produce)
 			}
 		}
