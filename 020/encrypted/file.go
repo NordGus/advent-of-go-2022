14340201fa1fe431d0ff7b1df5a4bda26e37e092
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2023-11-14 05:23:50 +0100

    day 20 part 1: solving bug

--- 020/encrypted/file.go
+++ 020/encrypted/file.go
@@ -6,19 +6,17 @@ const (
 
 type node struct {
 	moves int64
-	next  *node
-	prev  *node
+	left  *node
+	right *node
 }
 
 type File struct {
-	items  []*node
-	zeroAt int
+	items []*node
 }
 
 func New() File {
 	return File{
-		items:  make([]*node, 0, defaultFileSize),
-		zeroAt: -1,
+		items: make([]*node, 0, defaultFileSize),
 	}
 }
 
@@ -26,18 +24,17 @@ func (f *File) AddItem(item int64) {
 	n := &node{moves: item}
 
 	if len(f.items) == 0 {
-		n.next = n
-		n.prev = n
+		n.right = n
+		n.left = n
 	} else {
-		n.next = f.items[0]
-		n.prev = f.items[len(f.items)-1]
+		n.right = f.items[0]
+		n.left = f.items[len(f.items)-1]
+
+		f.items[0].left = n
+		f.items[len(f.items)-1].right = n
 	}
 
 	f.items = append(f.items, n)
-
-	if n.moves == 0 {
-		f.zeroAt = len(f.items) - 1
-	}
 }
 
 func (f *File) Size() int {
@@ -47,70 +44,56 @@ func (f *File) Size() int {
 func (f *File) MixFilePart1(coordinates ...int) int64 {
 	var (
 		result int64
+		zero   *node
+
+		m = int64(len(f.items) - 1)
 	)
 
 	for i := 0; i < len(f.items); i++ {
 		var (
 			current = f.items[i]
-			moves   = current.moves
 		)
 
-		if moves < 0 {
-			f.mixBackwards(current, -moves)
-		} else if moves > 0 {
-			f.mixForward(current, moves)
+		if current.moves == 0 {
+			zero = current
+			continue
 		}
+
+		p := current
+
+		if current.moves > 0 {
+			for i := int64(0); i < current.moves%m; i++ {
+				p = p.right
+			}
+		} else {
+			for i := int64(0); i < -(current.moves-1)%m; i++ {
+				p = p.left
+			}
+		}
+
+		if current == p {
+			continue
+		}
+
+		current.right.left = current.left
+		current.left.right = current.right
+
+		p.right.left = current
+		current.right = p.right
+
+		p.right = current
+		current.left = p
 	}
 
 	for i := 0; i < len(coordinates); i++ {
-		current := f.items[f.zeroAt]
+		current := zero
 
 		for j := 0; j < coordinates[i]; j++ {
-			current = current.next
+			current = current.right
 		}
 
 		result += current.moves
 	}
 
 	return result
-}
-
-// mixForward moves the value forward in the list.
-func (f *File) mixForward(from *node, moves int64) {
-	if moves == 0 {
-		return
-	}
-
-	var (
-		prev = from.prev
-		next = from.next
-	)
-
-	prev.next = next
-	next.prev = prev
-
-	from.next = next.next
-	from.prev = next
-
-	f.mixForward(from.next, moves-1)
-}
-
-// mixBackwards moves the value backwards in the list.
-func (f *File) mixBackwards(from *node, moves int64) {
-	if moves == 0 {
-		return
-	}
-
-	var (
-		prev = from.prev
-		next = from.next
-	)
-
-	prev.next = next
-	next.prev = prev
-
-	from.next = prev
-	from.prev = prev.prev
-
-	f.mixBackwards(from.prev, moves-1)
 }
